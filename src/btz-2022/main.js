0e3193c1df37f497658acf5f7e66b35bed5cc598
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-10-12 20:32:41 +0900

    アニメーションの終了処理を実装

--- src/btz-2022/main.js
+++ src/btz-2022/main.js
@@ -1,3 +1,19 @@
 import '@google/model-viewer'
 
-console.log('COMING SOON...')
+window.onload = () => {
+  // 実行したい処理を書く
+  setTimeout(fadeOutAnimation, 16000)
+}
+
+const fadeOutAnimation = () => {
+  // アニメーションをフェードアウトさせる
+  const animationElem = document.querySelector('#animation-wrapper')
+  animationElem.classList.add('end-animation')
+  setTimeout(destroyAnimation, 510)
+}
+
+const destroyAnimation = () => {
+  // アニメーションエリアを非表示にする
+  const animationElem = document.querySelector('#animation-wrapper')
+  animationElem.style.display = 'none'
+}
