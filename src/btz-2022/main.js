616c503509ab498250d125923e4376abdd4c7a55
s1F102100423 <85665559+s1F102100423@users.noreply.github.com>
2022-10-16 02:47:20 +0900

    アニメーションの付与
    モニターのフェードイン演出追加・モニターのアニメーションを改良しました

--- src/btz-2022/main.js
+++ src/btz-2022/main.js
@@ -1,3 +1,21 @@
 import '@google/model-viewer'
 
 console.log('COMING SOON...')
+
+var x = 0.01
+var y = 0.01
+var m = document.getElementById("about");
+
+window.onload = function load(){
+    document.querySelector('#about').animate(
+        [
+            { opacity: 0},
+            { opacity: 1}
+        ],
+        {
+            duration: 500,
+            fill: 'forwards'
+        
+        }
+    );
+}
