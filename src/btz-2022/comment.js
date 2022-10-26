75bb7ba9228eff93539a929f08f15bc7d4f972b5
s1F102100423 <85665559+s1F102100423@users.noreply.github.com>
2022-10-26 13:11:50 +0900

    フェードイン演出の実装
    フェードイン演出を追加しました。下ふたつは要素が3割見えたらフェードインするようになってます！

---
+++ src/btz-2022/comment.js
@@ -0,0 +1,74 @@
+window.onload = () => {
+    document.querySelector('.com1').animate(
+        [
+            { opacity: 0},
+            { opacity: 1}
+        ],
+        {
+            duration: 500,
+            fill: 'forwards'
+        
+        }
+    )
+}
+
+function scroll(entries) {
+
+    if (entries[0].intersectionRatio == 0) {
+    
+    return;
+    }
+    
+    else
+    {
+    elements2.classList.add("fadein");
+    
+    }
+    
+    }
+
+function scroll2(entries) {
+
+    if (entries[0].intersectionRatio == 0) {
+    
+    return;
+    }
+    
+    else
+    {
+    elements2.classList.add("fadein");
+    
+    }
+    
+    }
+
+function scroll3(entries) {
+
+        if (entries[0].intersectionRatio == 0) {
+        
+        return;
+        }
+        
+        else
+        {
+        elements3.classList.add("fadein");
+        
+        }
+        
+        }
+    
+    var options = {
+    
+    threshold:0.3
+    }
+    
+    var elements2 = document.querySelector(".com2");
+    var elements3 = document.querySelector(".com3");
+    
+    var observer2 = new IntersectionObserver(scroll2,options);
+    var observer3 = new IntersectionObserver(scroll3,options);
+    
+    observer2.observe(elements2);
+    observer3.observe(elements3);
+
+    
