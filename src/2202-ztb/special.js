2c04b4d1d39c6662800f4c8bb8f4ca5b510cc644
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-11-26 16:26:58 +0900

    デバッグ用の記述を削除

--- src/2202-ztb/special.js
+++ src/2202-ztb/special.js
@@ -1,48 +1,29 @@
 // APIでデータを受け取って表示する
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbz7F3xyHC-RunzHVuPT4hQEtCLMiHe5uHtK-ctqYX49-SfiMGXhu4arXiLAS66PqAMsFg/exec'
+const API_URL =
+  'https://script.google.com/macros/s/AKfycbz7F3xyHC-RunzHVuPT4hQEtCLMiHe5uHtK-ctqYX49-SfiMGXhu4arXiLAS66PqAMsFg/exec'
 
 const getData = async () => {
-    const res = await fetch(
-        API_URL,
-      {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-          Accept: 'application/json',
-        },
-      }
-    )
-    if (res.status !== 200) {
-      console.error('データの取得に失敗しました')
-    }
-    return res.json()
+  const res = await fetch(API_URL, {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+  if (res.status !== 200) {
+    console.error('データの取得に失敗しました')
   }
-
-/*
-const data = fetch()
-   .then((response) => {
-       return response.json()
-   })
-   .then((result) => {
-   })
-   .catch((e) => {
-       console.error(e)
-   })
-*/
+  return res.json()
+}
 
 window.onload = async () => {
-    const data = await getData()
-    console.log(data);
-    const i = document.getElementById("t");
-    // for(let sec=0; sec<length(data);sec++){
-    for(const item of data){
-        const n = document.createElement("p");
-        n.textContent = item;
-        n.classList.add("story-section");
-        // const content = document.createTextNode(item);
-        i.appendChild(n);
-
-    }
-    // i.textContent= data;
+  const data = await getData()
+  const i = document.getElementById('t')
+  for (const item of data) {
+    const n = document.createElement('p')
+    n.textContent = item
+    n.classList.add('story-section')
+    i.appendChild(n)
+  }
 }
